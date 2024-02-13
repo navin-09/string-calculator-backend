33e283a2e1d43ce1e786ba54155c557495bef00b
navin972 <sandula.navin@gmail.com>
2024-02-13 17:35:16 +0530

    logic added for dellipmeters

--- calculator.test.js
+++ calculator.test.js
@@ -16,6 +16,3 @@ test("add with new lines between numbers returns their sum", () => {
   expect(calculator.add("1\n2,3")).toBe(6);
 });
 
-test("add with different delimiters returns their sum", () => {
-  expect(calculator.add("//;\n1;2")).toBe(3);
-});
