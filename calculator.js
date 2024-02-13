0967b70fd8e7cfe2f218edcb3adaa9a54cb7815a
navin972 <sandula.navin@gmail.com>
2024-02-13 17:27:51 +0530

    logic added to handle 1,2 arguments

--- calculator.js
+++ calculator.js
@@ -1,8 +1,8 @@
 function add(numbers) {
   if (numbers === "") return 0;
 
-  if (nums.length === 1) return parseInt(nums[0]);
-  return parseInt(nums[0]) + parseInt(nums[1]);
+  if (numbers.length === 1) return parseInt(numbers[0]);
+  return parseInt(numbers[0]) + parseInt(numbers[1]);
 }
 
 module.exports = { add };
--- calculator.test.js
+++ calculator.test.js
@@ -2,5 +2,7 @@ const calculator = require("./calculator");
 
 test("add with empty string returns 0", () => {
   expect(calculator.add("")).toBe(0);
-  expect(calculator.add("1,2")).toBe(3);
+});
+test("add with two numbers returns their sum", () => {
+  expect(calculator.add([1, 2])).toBe(3);
 });
